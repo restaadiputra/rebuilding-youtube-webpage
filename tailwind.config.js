1bf6bb36a78ca1aeecd4854b33397d56344957bf
Resta Adiputra <resta.adiputra@myboost.id>
2020-02-22 20:12:08 +0700

    Add header logo and botton menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          'Roboto',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       spacing: {
         '14': '3.5rem',
         '60': '15rem',
       },
       colors: {
         'gray-100-youtube': '#F9F9F9',
+      },
+      fontSize: {
+        '2xs': '0.625rem',
+        '3xs': '0.5rem'
       }
     },
   },
